23d2c7b489213e1abaf08a60b0af7ca5dc6b1eee
Marco Laptop <mpoon25@my.bcit.ca>
2022-11-08 11:06:53 -0800

    javascript exercises

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,31 @@
-for (let i = 1; 2**i <= 1024; i++){
-    console.log(2**i)
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+};
+
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+};
+
+let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+};
+
+let students = [student1, student2, student3];
+
+let counter = 0
+for (let student of students){
+    if (student.GPA > 3 && student.program === 'CIT'){
+        counter++
+        for(attribute in student){
+            console.log(`${student[attribute]}`)
+        }
+    }
 }
 
-let i = 1
-while(2**i <= 1024){
-    console.log(2**i)   
-    i++
-}
+console.log(`counter: ${counter}`)
